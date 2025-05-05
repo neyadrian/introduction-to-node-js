7d0dcbada874a0b761344d405da3054ea6befd8b
Ney Adrian <neyadrian2018@gmail.com>
2025-05-05 13:06:47 -0300

    Atualizacao

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,11 @@
 import express from 'express';
+import produtosRouter from './produtos';
+import voosRouter from './voos';
 
 const router = express.Router();
+
+router.use('/produtos', produtosRouter);
+router.use('/voos', voosRouter);
 
 router.get('/ping', (req, res) => {
     res.json({ pong: true });
--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import express from 'express';
 import helmet from 'helmet';
 import path from 'path';
 import router from './routes';
-import produtosRouter from './routes/produtos';
-import voosRouter from './routes/voos';
 
 const server = express();
 
@@ -12,8 +10,6 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static(path.join(__dirname, '../public')));
 
-server.use('/produtos', produtosRouter);
-server.use('/voos', voosRouter);
 server.use('/', router);
 
 server.listen(3000, () => {
